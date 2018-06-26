840644a7e830809cd005b940648473030905561d
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-06-26 16:04:17 -0700

    shadow landcapes

--- src/components/LandCard/LandCard.js
+++ src/components/LandCard/LandCard.js
@@ -3,10 +3,16 @@ import "./LandCard.css";
 
 const LandCard = props => (
   <div className="card " onClick={() => props.cardClick(props.id)}>
-    <div className="img-container">
       <img alt={props.id} src={props.image} />
-    </div>
   </div>
 );
+
+
+<div class="hover01 column">
+  <div>
+    <figure><img src="https://nxworld.net/example/css-image-hover-effects/pic01.jpg" /></figure>
+    <span>Hover</span>
+  </div>
+</div>
 
 export default LandCard;
